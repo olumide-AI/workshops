7eb91e91e51c9c16e9241d2e2ae13bfac560b2e5
olumide-AI <olumidekolawole26@gmail.com>
2025-04-15 16:01:15 -0700

    inital commit with only thr get monthly payout method created

--- financial-calculators/FinancialCalculator/src/com/pluralsight/olumide/financialcalculator/PresentValueCalculator.java
+++ financial-calculators/FinancialCalculator/src/com/pluralsight/olumide/financialcalculator/PresentValueCalculator.java
@@ -1,4 +1,22 @@
 package com.pluralsight.olumide.financialcalculator;
 
+import java.security.PublicKey;
+import java.util.Scanner;
+
 public class PresentValueCalculator {
+    public static double getMonthlyPayout(Scanner scanner){
+        System.out.println("What is your monthly payout: ");
+        return scanner.nextDouble();
+    }
+    public  static double getInterestRate(Scanner scanner){
+        System.out.println("What is your interest rate ");
+    }
+
+    public static void main(String[] args) {
+        //Initialize scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //User inputs
+        double monthlyPayout = getMonthlyPayout(scanner);
+    }
 }
