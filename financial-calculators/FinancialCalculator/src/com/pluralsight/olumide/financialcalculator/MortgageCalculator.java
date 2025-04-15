77f180b3399052e84470d80319e7cd3c529c76f3
olumide-AI <olumidekolawole26@gmail.com>
2025-04-15 11:28:51 -0700

    i have created a scanner class, and created the methods to get the principal, i set interest rate as fixed using a static method and also getting the loan term from the user

--- financial-calculators/FinancialCalculator/src/com/pluralsight/olumide/financialcalculator/MortgageCalculator.java
+++ financial-calculators/FinancialCalculator/src/com/pluralsight/olumide/financialcalculator/MortgageCalculator.java
@@ -1,4 +1,32 @@
 package com.pluralsight.olumide.financialcalculator;
 
+import java.util.Scanner;
+
 public class MortgageCalculator {
+    //Create scanner class for user input for readability
+    static Scanner scanner = new Scanner(System.in);
+    static double annualIntRate = 0.07625;
+
+    //Get principal, interest rate and loan length from user
+    //Get Principal
+    public static double getPrincipalAmount() {
+        System.out.println("Please provide your Principal amount: ");
+        double principal = scanner.nextDouble();
+        return principal;
+    }
+    //Interest rate is predefined
+    public static double getMonthlyIntRate() {
+        double monthlyIntRate = annualIntRate/12;
+        System.out.println("Your monthly interest rate: " + monthlyIntRate);
+        return monthlyIntRate;
+    }
+    //Length of loan
+    public static int getLoanTerm(){
+        System.out.println("What is your loan term: ");
+        int loanTerm = scanner.nextInt();
+        return loanTerm;
+    }
+
+    //Logic calculations 
+
 }
